5188087b4a7735493b68cc334182ecc58edd7c2d
Shrirang <shrirangthatsit10@gmail.com>
2023-04-14 18:21:16 +0530

    added data

--- analyzer.py
+++ analyzer.py
@@ -3,7 +3,7 @@ import csv
 final_reviews = []
 text = ""
 
-with open('metrics1.csv', 'r') as file:
+with open('metrics3.csv', 'r') as file:
     reader = csv.reader(file)
     count1 = 0
     count2 = 0
@@ -14,19 +14,19 @@ with open('metrics1.csv', 'r') as file:
     count7 = 0
     for document in reader:
         print(document[0], document[1])
-        if (float(document[1]) < 0.1):  # very negative
+        if (float(document[1]) >= 0 and float(document[1]) < 0.1):  # very negative
             text = "Worst"
             count1 += 1
-        elif (float(document[1]) >= 0.1 and float(document[1]) < 0.2):  # negative
+        elif (float(document[1]) >= 0.1 and float(document[1]) < 0.15):  # negative
             text = "Very Poor"
             count2 += 1
-        elif (float(document[1]) >= 0.2 and float(document[1]) < 0.25):  # partially negative
+        elif (float(document[1]) >= 0.15 and float(document[1]) < 0.20):  # partially negative
             text = "Poor"
             count3 += 1
-        elif (float(document[1]) >= 0.25 and float(document[1]) < 0.3):  # mixed
+        elif (float(document[1]) >= 0.20 and float(document[1]) < 0.25):  # mixed
             text = "Average"
             count7 += 1
-        elif (float(document[1]) >= 0.3 and float(document[1]) < 0.4):  # partially positive
+        elif (float(document[1]) >= 0.25 and float(document[1]) < 0.4):  # partially positive
             text = "Good"
             count4 += 1
         elif (float(document[1]) >= 0.4 and float(document[1]) < 0.5):  # positive
@@ -41,7 +41,8 @@ with open('metrics1.csv', 'r') as file:
 print(count1, count2, count3, count7, count4, count5, count6)
 
 
-with open('metrics1.csv', 'w') as file:
+with open('metrics.csv', 'w') as file:
     writer = csv.writer(file)
+    writer.writerow(["id","metric","review"])
     for val in final_reviews:
         writer.writerow([val[0], val[1], val[2]])
